807a6ec6f39746334d8b0f4b8213e74739d3c679
Sean Welch <seanwelch51199@icloud.com>
2023-06-29 17:27:35 -0300

    added s3 client example

---
+++ src/lib/aws.ts
@@ -0,0 +1,28 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import multer from 'multer';
+
+const bucketName = process.env.BUCKET_NAME;
+const bucketRegions = process.env.BUCKET_REGION;
+const awsAccessKey = process.env.AWS_ACCESS_KEY as string;
+const awsSecretKey = process.env.AWS_SECRET_KEY as string;
+
+const s3 = new S3Client({
+    region: bucketRegions,
+    credentials: {
+        accessKeyId: awsAccessKey,
+        secretAccessKey: awsSecretKey,
+    },
+});
+
+const exampleCode = async () => {
+    const params = {
+        Bucket: bucketName,
+        Key: 'test.txt',
+        Body: 'Hello World',
+        ContentType: 'text/plain',
+    };
+
+    const command = new PutObjectCommand(params);
+
+    await s3.send(command);
+};
