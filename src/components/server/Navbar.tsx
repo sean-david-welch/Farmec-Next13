5df5a948edf9fc766cb9bf22e38a621b12e71979
Sean Welch <seanwelch51199@icloud.com>
2023-06-30 12:35:38 -0300

    some navbar styling

--- src/components/server/Navbar.tsx
+++ src/components/server/Navbar.tsx
@@ -1,9 +1,39 @@
+'use client';
+
 import styles from '../styles/Header.module.css';
+import Link from 'next/link';
+
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
     return (
         <nav className={styles.navbar}>
             <h1>Farmec</h1>
+            <ul className={styles.navList}>
+                <li
+                    className={styles.navItem}
+                    onMouseEnter={() => setOpen(true)}
+                    onMouseLeave={() => setOpen(false)}>
+                    <Link href={'/'}>Hey</Link>
+
+                    <ul
+                        className={
+                            open ? styles.navDrop : styles.navDropClosed
+                        }>
+                        <li className={styles.navDropItem}>
+                            <Link href={'/'}>hi</Link>
+                        </li>
+                        <li className={styles.navDropItem}>
+                            <Link href={'/'}>hi</Link>
+                        </li>
+                        <li className={styles.navDropItem}>
+                            <Link href={'/'}>hi</Link>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </nav>
     );
 };
