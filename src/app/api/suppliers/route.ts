8c8079db1123679a5dfc6e143aba5607a5b22024
Sean Welch <seanwelch51199@icloud.com>
2023-07-01 19:30:19 -0300

    some s3 config

--- src/app/api/suppliers/route.ts
+++ src/app/api/suppliers/route.ts
@@ -1,5 +1,6 @@
-import { NextResponse } from 'next/server';
 import { prisma } from '~/lib/prisma';
+import { NextResponse, NextRequest } from 'next/server';
+import { validateUser, errorResponse } from '~/lib/utils';
 
 export const GET = async () => {
     const projects = await prisma.supplier.findMany();
@@ -7,4 +8,28 @@ export const GET = async () => {
     return NextResponse.json(projects);
 };
 
-export const POST = async () => {};
+export const POST = async (request: NextRequest) => {
+    try {
+        await validateUser(request);
+        const data = await request.json();
+
+        const supplier = await prisma.supplier.create({
+            data: {
+                name: data.name,
+                logo_image: data.logo_image,
+                marketing_image: data.marketing_image,
+                description: data.description,
+                social_facebook: data.social_facebook,
+                social_twitter: data.social_twitter,
+                social_instagram: data.social_instagram,
+                social_youtube: data.social_youtube,
+                social_linkedin: data.social_linkedin,
+                social_website: data.social_website,
+            },
+        });
+
+        return NextResponse.json(supplier);
+    } catch (error: any) {
+        return errorResponse(500, error.message || 'Internal Server Error');
+    }
+};
--- src/lib/aws.ts
+++ src/lib/aws.ts
@@ -4,32 +4,61 @@ import {
     DeleteObjectCommand,
     GetObjectCommand,
 } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
 import multer from 'multer';
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const bucketName = process.env.BUCKET_NAME;
 const bucketRegions = process.env.BUCKET_REGION;
 const awsAccessKey = process.env.AWS_ACCESS_KEY as string;
 const awsSecretKey = process.env.AWS_SECRET_KEY as string;
 
-const s3 = new S3Client({
+const clientParams = {
     region: bucketRegions,
     credentials: {
         accessKeyId: awsAccessKey,
         secretAccessKey: awsSecretKey,
     },
-});
+};
+const s3 = new S3Client(clientParams);
 
-const exampleCode = async () => {
-    const params = {
+export function uploadFile(
+    fileBuffer: string,
+    fileName: string,
+    mimetype: string
+) {
+    const uploadParams = {
         Bucket: bucketName,
-        Key: 'test.txt',
-        Body: 'Hello World',
-        ContentType: 'text/plain',
+        Body: fileBuffer,
+        Key: fileName,
+        ContentType: mimetype,
     };
 
-    const command = new PutObjectCommand(params);
+    return s3.send(new PutObjectCommand(uploadParams));
+}
 
-    // Get signed url
+export function deleteFile(fileName: string) {
+    const deleteParams = {
+        Bucket: bucketName,
+        Key: fileName,
+    };
 
-    await s3.send(command);
-};
+    return s3.send(new DeleteObjectCommand(deleteParams));
+}
+
+// export async function getObjectSignedUrl(key: string) {
+//     const params = {
+//         Bucket: bucketName,
+//         Key: key,
+//     };
+
+//     // https://aws.amazon.com/blogs/developer/generate-presigned-url-modular-aws-sdk-javascript/
+//     const command = new GetObjectCommand(params);
+//     const seconds = 60;
+//     const url = await getSignedUrl(s3, command, { expiresIn: seconds });
+
+//     return url;
+// }
