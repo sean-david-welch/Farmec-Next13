92015909f0fdf4d3ea63bb9ecf753ec3da7ef1e2
Sean Welch <seanwelch51199@icloud.com>
2023-07-02 15:14:40 -0300

    dynamic import for navbar

--- src/components/server/Header.tsx
+++ src/components/server/Header.tsx
@@ -1,5 +1,7 @@
-import Navbar from '../client/Navbar';
+import dynamic from 'next/dynamic';
 import { getSessionAndUser } from '~/lib/utils';
+
+const Navbar = dynamic(() => import('../client/Navbar'));
 
 const Header = async () => {
     const { user, session } = await getSessionAndUser();
