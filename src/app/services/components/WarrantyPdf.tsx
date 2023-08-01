18976127b7ce306a50763f39cb616d79660c594c
Sean Welch <seanwelch51199@icloud.com>
2023-08-01 09:06:00 -0400

    cloudflare tunrstile

---
+++ src/app/services/components/WarrantyPdf.tsx
@@ -0,0 +1,25 @@
+// import { Page, Text, View, Document, StyleSheet, PDFDownloadLink } from '@react-pdf/renderer';
+
+// // Create styles
+// const styles = StyleSheet.create({
+//   page: {
+//     flexDirection: 'row',
+//     backgroundColor: '#E4E4E4'
+//   },
+//   section: {
+//     margin: 10,
+//     padding: 10,
+//     flexGrow: 1
+//   }
+// });
+
+// const WarrantyPDF = ({ warranty, parts }) => (
+//   <Document>
+//     <Page size="A4" style={styles.page}>
+//       <Text>Warranty Claim: {warranty.dealer} - {warranty.owner_name}</Text>
+//       // Add more structure here...
+//     </Page>
+//   </Document>
+// );
+
+// export default WarrantyPDF;
--- src/components/client/ContactForm.tsx
+++ src/components/client/ContactForm.tsx
@@ -1,30 +1,35 @@
 'use client';
 
 import axios from 'axios';
+import Script from 'next/script';
 import utils from '~/styles/Utils.module.css';
 
-import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-
 import { cloudflareKey } from '~/lib/config';
-import { Turnstile } from '@marsidev/react-turnstile';
 
 const ContactForm = () => {
     const router = useRouter();
-    const [token, setToken] = useState<string>('');
 
     async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const formData = new FormData(event.currentTarget as HTMLFormElement);
+        const turnstileResponse = formData.get('cf-turnstile-token') as string;
+
+        if (!turnstileResponse) {
+            console.error('No turnstile response');
+            return;
+        }
 
         const body = {
             name: formData.get('name'),
             email: formData.get('email'),
             message: formData.get('message'),
-            token: token,
+            token: turnstileResponse,
         };
 
-        const verificationResponse = await axios.post('/api/verify', { token });
+        const verificationResponse = await axios.post('/api/verify', body);
+
+        console.log(verificationResponse.data);
 
         if (verificationResponse.data.success) {
             await axios.post('/api/contact', body);
@@ -34,28 +39,34 @@ const ContactForm = () => {
         }
     }
     return (
-        <form onSubmit={handleSubmit} className={utils.contactForm}>
-            <label htmlFor="name">Name:</label>
-            <input type="text" required={true} placeholder="name" />
-            <label htmlFor="email">Email:</label>
-            <input type="email" required={true} placeholder="email" />
-            <label htmlFor="message">Message:</label>
-            <textarea
-                name="message"
-                placeholder="Enter your message here..."
-                cols={30}
-                rows={10}
-                required={true}
+        <>
+            <Script
+                src="https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback"
+                strategy="lazyOnload"
             />
-            <Turnstile
-                siteKey={cloudflareKey}
-                onSuccess={token => setToken(token)}
-            />
+            <form onSubmit={handleSubmit} className={utils.contactForm}>
+                <label htmlFor="name">Name:</label>
+                <input type="text" required={true} placeholder="name" />
+                <label htmlFor="email">Email:</label>
+                <input type="email" required={true} placeholder="email" />
+                <label htmlFor="message">Message:</label>
+                <textarea
+                    name="message"
+                    placeholder="Enter your message here..."
+                    cols={30}
+                    rows={10}
+                    required={true}
+                />
 
-            <button className={utils.btnForm} type="submit">
-                Submit
-            </button>
-        </form>
+                <div
+                    className="cf-turnstile"
+                    data-sitekey={cloudflareKey}></div>
+
+                <button className={utils.btnForm} type="submit">
+                    Submit
+                </button>
+            </form>
+        </>
     );
 };
 
