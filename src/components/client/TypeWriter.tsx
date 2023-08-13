fa713826a57b7d29865dbea91969cba5d70d06ff
Sean Welch <seanwelch51199@icloud.com>
2023-08-13 09:45:15 +0100

    typewriter reduce cls

--- src/components/client/TypeWriter.tsx
+++ src/components/client/TypeWriter.tsx
@@ -4,37 +4,50 @@ import utils from '~/styles/Utils.module.css';
 import Link from 'next/link';
 import TypewriterComponent from 'typewriter-effect';
 
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
 
 const Typewriter = () => {
-    return (
-        <div className={utils.typewriter}>
-            <h1>
-                <TypewriterComponent
-                    options={{
-                        loop: false,
-                        cursor: '',
-                        delay: 50,
-                    }}
-                    onInit={typewriter => {
-                        typewriter
-                            .stop()
-                            .typeString(
-                                'Importers & Distributors of Quality Agricultural Machinery'
-                            )
-                            .start();
-                    }}
-                />
-            </h1>
+    const [isClient, setIsClient] = useState(false);
 
-            <button className={utils.btn}>
-                <Link href={'#Info'}>
-                    Find Out More:
-                    <FontAwesomeIcon icon={faChevronCircleDown} />
-                </Link>
-            </button>
-        </div>
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    return (
+        <>
+            {!isClient ? (
+                <div className={utils.typewriterSkeleton}></div>
+            ) : (
+                <div className={utils.typewriter}>
+                    <h1>
+                        <TypewriterComponent
+                            options={{
+                                loop: false,
+                                cursor: '',
+                                delay: 50,
+                            }}
+                            onInit={typewriter => {
+                                typewriter
+                                    .stop()
+                                    .typeString(
+                                        'Importers & Distributors of Quality Agricultural Machinery'
+                                    )
+                                    .start();
+                            }}
+                        />
+                    </h1>
+
+                    <button className={utils.btn}>
+                        <Link href={'#Info'}>
+                            Find Out More:
+                            <FontAwesomeIcon icon={faChevronCircleDown} />
+                        </Link>
+                    </button>
+                </div>
+            )}
+        </>
     );
 };
 
